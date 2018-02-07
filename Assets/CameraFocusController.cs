ea3c13219d0c5f79881decc8e2d25b04b33367bb
An Nguyen <francisnguyen1201@gmail.com>
2018-02-07 16:36:54 -0500

    Auto focus implemented.
    A C# script to set AR camera to continuously focus.

---
+++ Assets/CameraFocusController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class CameraFocusController : MonoBehaviour {
+	void Start ()
+	{
+		Debug.Log ("I'm alive");
+		//var vuforia = VuforiaARController.Instance;
+		//vuforia.RegisterVuforiaStartedCallback(OnVuforiaStarted);
+		//vuforia.RegisterOnPauseCallback(OnPaused);
+
+		VuforiaARController.Instance.RegisterVuforiaStartedCallback(OnVuforiaStarted); 
+		VuforiaARController.Instance.RegisterOnPauseCallback(OnPaused);
+	
+	}
+
+	private void OnVuforiaStarted()
+	{
+		CameraDevice.Instance.SetFocusMode(
+			CameraDevice.FocusMode.FOCUS_MODE_CONTINUOUSAUTO);
+	}
+
+	private void OnPaused(bool paused)
+	{
+		if (!paused) // resumed
+		{
+			// Set again autofocus mode when app is resumed
+			CameraDevice.Instance.SetFocusMode(
+				CameraDevice.FocusMode.FOCUS_MODE_CONTINUOUSAUTO);
+		}
+	}
+
+}
